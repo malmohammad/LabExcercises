500cd778be14528edba33a2eb6b5153dce2abe21
malmohammad <mak_almo@yahoo.com>
2020-07-15 18:09:49 -0400

    lab21

---
+++ Lab21/Lab21/Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using Lab21.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Registration registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(registration);
+        }
+    }
+}
---
+++ Lab21/Lab21/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab21.Models
+{
+    public class Registration
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
